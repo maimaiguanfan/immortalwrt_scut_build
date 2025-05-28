3bd7d0daad9f3e22aabbb59c5cbfc7c3dae3d84c
maimaiguanfan <maimaiguanfan@gmail.com>
2025-05-28 21:50:23 +0800

    Build my package

--- 01_prepare.sh
+++ 01_prepare.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+./scripts/feeds update -a
 
-./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
-rm -rf feeds/packages/utils/v2dat
-sed -i '20i PKG_USE_MIPS16:=0' feeds/small/dns2tcp/Makefile
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+#拉取istore界面和ddnsto
+rm -rf package/istore/
+mkdir package/istore/
+git clone https://github.com/linkease/istore.git package/istore/luci-app-store --depth=1
+git clone https://github.com/linkease/nas-packages.git ../nas-packages --depth=1
+git clone https://github.com/linkease/nas-packages-luci.git ../nas-packages-luci --depth=1
+cp -r ../nas-packages/network/services/quickstart package/istore/
+cp -r ../nas-packages/network/services/ddnsto package/istore/
+cp -r ../nas-packages-luci/luci/luci-app-quickstart package/istore/
+cp -r ../nas-packages-luci/luci/luci-app-ddnsto package/istore/
 
-rm -rf feeds/packages/net/frp
-git clone https://github.com/kuoruan/openwrt-frp.git package/frp
+#拉取wake on lan plus
+git clone https://github.com/animegasan/luci-app-wolplus.git package/luci-app-wolplus --depth=1
+
+#单独拉取vernesong版OpenClash
+ rm -rf feeds/luci/applications/luci-app-openclash/
+ git clone https://github.com/vernesong/OpenClash.git feeds/luci/applications/luci-app-openclash --depth=1
 
 #单独拉取scutclient
 rm -rf feeds/luci/applications/luci-app-scutclient/
-git clone https://github.com/maimaiguanfan/luci-app-scutclient.git feeds/luci/applications/luci-app-scutclient 
+git clone https://github.com/maimaiguanfan/luci-app-scutclient.git feeds/luci/applications/luci-app-scutclient --depth=1
 
 #单独拉取联通加速
 mkdir package/scut-unicom
@@ -22,12 +29,8 @@ wget https://raw.githubusercontent.com/wykdg/route_script/master/scut-unicom/Mak
 
 ./scripts/feeds install -a
 
-
 #在启动项加入联通加速
 sed -i '3i #如果使用联通加速，删除前面的#，并填充后面的值\n#sleep 10 && /usr/share/scut_unicom/add_route.sh server_ip username password' package/base-files/files/etc/rc.local 
-#将ttyd改成默认root登录
-sed -i "s/option command '\/bin\/login'/option command '\/bin\/login -f root'/" feeds/packages/utils/ttyd/files/ttyd.config 
-
 
 sed  -i "s/exit 0/[ ! -f '\/usr\/sbin\/trojan' ] \&\& [ -f '\/usr\/bin\/trojan-go' ] \&\& ln -sf \/usr\/bin\/trojan-go \/usr\/bin\/trojan\nexit 0/" package/emortal/default-settings/files/99-default-settings                
 
